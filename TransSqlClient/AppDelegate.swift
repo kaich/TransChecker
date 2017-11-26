367a5b816100843f404134b7bb5e2604fd720edd
kai <chengkai1853@163.com>
2017-11-26 15:05:08 +0800

    Initial Commit

---
+++ TransSqlClient/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TransSqlClient
+//
+//  Created by kai on 2017/11/26.
+//  Copyright © 2017年 kai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TransSqlClient/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TransSqlClient
+//
+//  Created by kai on 2017/11/26.
+//  Copyright © 2017年 kai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
